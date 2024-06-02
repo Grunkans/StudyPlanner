cacdeb6b57699b8fa37eaadf76d6df0e7b6a948a
Grunkans <143597576+Grunkans@users.noreply.github.com>
2024-06-02 22:01:47 +0200

    testat fyra, söka efter todo. klart

---
+++ cypress/e2e/prioList.cy.js
@@ -0,0 +1,17 @@
+describe('day' , () => {
+
+
+	//Som en student vill jag kunna söka efter todo items som innehåller en viss text, så att jag lätt kan se om en viss sak finns med i priolistan.
+
+	it('should be able to show me if the word Im searching for appears in any todos' , () => {
+		cy.visit('/')
+		cy.get('[ data-cy="search-input" ]').click()
+		cy.get('[ data-cy="search-input" ]').type("9-16")
+		cy.get('[ data-cy="search-results" ]').should('not.contain', 'inlämning')
+		cy.get('[ data-cy="search-results" ]').should('contain', 'Lektion i skolan 9-16')
+
+		
+	})
+
+	
+})
--- src/components/prio-list/PrioList.jsx
+++ src/components/prio-list/PrioList.jsx
@@ -1,19 +1,33 @@
 import PrioItem from "./PrioItem"
 import { useStore } from '../../data/store.js'
+import React, { useState } from "react"
+
 
 const PrioList = () => {
+
+	const [searchTerm, setSearchTerm] = useState('')
 	const todos = useStore(state => state.todos)
-	const items = todos.filter(t => !t.done)
+	// const items = todos.filter(t => !t.done)
+
+	const filteredTodos = todos.filter(todo => {
+		return todo.text.toLowerCase().includes(searchTerm.toLowerCase());
+	  });
+	
 
 	return (
 		<div className="prio-list">
 			<h2> Vad ska jag göra nu? </h2>
 			<div className="list-container">
-				<input type="search" placeholder="Filtrera uppgifter" />
+				<input 
+				data-cy="search-input"
+				type="search" placeholder="Filtrera uppgifter"
+				value={searchTerm} 
+				onChange={(e) => setSearchTerm(e.target.value)} />
 
-				<div className="prio-items">
-					{items.map((item, index) => (
-						<PrioItem key={item.id} item={item} num={index+1} />
+			<div className="prio-items"
+					data-cy="search-results">
+					{filteredTodos.map((item, index) => (
+						<PrioItem key={item.id} item={item} num={index + 1} />
 					))}
 				</div>
 
